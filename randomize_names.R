830f318524da0335110bfb5f4dd29990e3573eb4
Gerard Cramer <gerard@dairycowfootdoc.com>
2025-11-01 16:25:55 -0500

    randomize names

---
+++ randomize_names.R
@@ -0,0 +1,7 @@
+# randomize names
+names <- c("Luciano", "Mark", "Dan", "Sam", "Sarah", "Bryan")
+randomized_names <- sample(names)
+# add commas
+cat(paste(randomized_names, collapse = ", "))
+
+# print(randomized_names)
